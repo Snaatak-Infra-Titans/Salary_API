66a0a964ee393410024a7dd6595775ee712ae63f
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-18 17:49:56 +0530

    Update SalaryApplicationTests.java
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -3,19 +3,12 @@ package com.opstree.microservice.salary;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.cassandra.core.CassandraTemplate;
 
 @Tag("integration")
 @SpringBootTest
 class SalaryApplicationTests {
 
-    // This tricks Spring Boot into thinking it has a database connection
-    @MockBean
-    private CassandraTemplate cassandraTemplate;
-
     @Test
     void contextLoads() {
-        // Now this will PASS successfully without a real DB!
     }
 }
